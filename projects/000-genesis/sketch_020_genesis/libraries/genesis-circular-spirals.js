e3ff3ae80f6099068373994b627a682c33530d02
Dennis Porter Jr <dennisporterjr@Denniss-MacBook-Pro.local>
2023-04-04 21:31:25 -0400

    gnss 20. loading animations separately...

---
+++ projects/000-genesis/sketch_020_genesis/libraries/genesis-circular-spirals.js
@@ -0,0 +1,119 @@
+class CircularSpiralAnimation extends Animation {
+    constructor() {
+      super();
+      this.numKeys = 88;
+      this.lowestMidiNote = 21;
+      this.highestMidiNote = 108;
+      this.keyColors = [
+        color(255, 0, 0), // Red
+        color(255, 127, 0), // Orange
+        color(255, 255, 0), // Yellow
+        color(0, 255, 0), // Green
+        color(0, 0, 255), // Blue
+        color(75, 0, 130), // Indigo
+        color(148, 0, 211), // Violet
+      ];
+      this.angle = 0;
+      this.circlePositions = [];
+    }
+  
+    setup() {
+      // Code specific to CircularSpiralAnimation setup (if any) can be added here
+    }
+  
+    update() {
+      if (this.hasFadingCircles()) {
+        this.updateCircles();
+      }
+    }
+
+    onKeyPress(note, velocity, color) {
+      this.drawCircle(note);
+    }
+
+    onKeyRelease(note, velocity) {
+      this.clearCircle(note);
+    }
+  
+    clearCircle(pitch) {
+      let position = this.circlePositions[pitch];
+      if (position) {
+        position.fadeStartTime = millis();
+        position.isFading = true;
+      }
+    }
+  
+    drawCircle(pitch) {
+      let index = pitch - this.lowestMidiNote;
+      if (index >= 0 && index < this.numKeys) {
+        let colorPos = map(index, 0, this.numKeys - 1, 0, this.keyColors.length - 1);
+        let keyColorIndex1 = int(colorPos);
+        let keyColorIndex2 = min(keyColorIndex1 + 1, this.keyColors.length - 1);
+        let lerpAmt = colorPos - keyColorIndex1;
+    
+        let noteColor = lerpColor(this.keyColors[keyColorIndex1], this.keyColors[keyColorIndex2], lerpAmt);
+    
+        // Calculate the circle's position using sine and cosine functions
+        let radius = map(index, 0, this.numKeys - 1, 0, min(width, height) / 2);
+        let x = width / 2 + radius * cos(this.angle);
+        let y = height / 2 + radius * sin(this.angle);
+    
+        // Increment the this.angle for the next circle
+        this.angle += radians(10);
+    
+        // Save the circle's position
+        this.circlePositions[pitch] = { x: x, y: y, color: noteColor, isFading: false };
+    
+        fill(noteColor);
+        noStroke();
+        ellipse(x, y, 50, 50);
+        redraw();
+      }
+    }
+  
+    updateCircles() {
+      background(0); // Eggshell color
+      for (let pitch in this.circlePositions) {
+        let position = this.circlePositions[pitch];
+        if (position.isFading) {
+          let elapsed = millis() - position.fadeStartTime;
+          let fadeDuration = 1000; // 1 second
+
+          if (elapsed >= fadeDuration) {
+            // The circle has fully faded, remove it
+            delete this.circlePositions[pitch];
+          } else {
+            // Update the circle's opacity
+            let alpha = map(elapsed, 0, fadeDuration, 255, 0);
+            let fadedColor = color(red(position.color), green(position.color), blue(position.color), alpha);
+
+            // Calculate the new radius
+            let newRadius = map(elapsed, 0, fadeDuration, 50, 10);
+
+            // Calculate the falling distance with easing
+            let easing = 0.5 * pow(elapsed / fadeDuration, 2);
+            let newY = position.y + (height - position.y) * easing;
+
+            fill(fadedColor);
+            noStroke();
+            ellipse(position.x, newY, newRadius, newRadius);
+          }
+        } else {
+          // If the circle is not fading, draw it with the original color
+          fill(position.color);
+          noStroke();
+          ellipse(position.x, position.y, 50, 50);
+        }
+      }
+    }
+  
+    hasFadingCircles() {
+      for (let pitch in this.circlePositions) {
+        if (this.circlePositions[pitch].isFading) {
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+  
---
+++ projects/000-genesis/sketch_020_genesis/libraries/genesis-intro.js
@@ -0,0 +1,117 @@
+let animation;
+let appState = "intro";
+
+let animations = [
+  { source: "vertical-bar.js", name: "Vertical Bars", class: VerticalBarAnimation },
+  { source: "circular-spiral.js", name: "Circular Spirals", class: CircularSpiralAnimation },
+];
+
+let genesisText = "GENESIS";
+let initialSpacing = 15;
+let introElapsedTime = 0;
+let introLetterTimes = new Array(genesisText.length).fill(0).map((_, i) => i * 161);
+let introDuration = 10000;
+let introComplete = false;
+let introFadeSpeed = 300;
+let animationOptionsDelay = 1618; // Time in milliseconds
+
+function easeOutQuint(t) {
+  return 1 - Math.pow(1 - t, 5);
+}
+
+function introSetup() {
+  // Setup variables and initialize p5.Typewriter library (if needed)
+}
+
+function introUpdate() {
+  background(0);
+  textAlign(CENTER, CENTER);
+  textSize(24); // Updated text size
+  noStroke();
+
+  let progress = introElapsedTime / introDuration;
+  let easedProgress = easeOutQuint(progress);
+  let letterSpacing = map(easedProgress, 0, 1, 0, 20);
+
+  // Draw the "GENESIS" text
+  for (let i = 0; i < genesisText.length; i++) {
+    let opacity = 0;
+    let letterTime = millis() - introLetterTimes[i];
+    if (letterTime >= 0) {
+      opacity = map(letterTime, 0, introFadeSpeed, 0, 255); // Updated fade-in speed
+    }
+    fill(255, opacity);
+    let x = width / 2 - (genesisText.length * (letterSpacing + initialSpacing)) / 2 + i * (letterSpacing + initialSpacing);
+    text(genesisText[i], x, height / 2 - 50);
+  }
+
+  introElapsedTime += deltaTime;
+  if (introElapsedTime >= introDuration && !introComplete) {
+    introComplete = true;
+  }
+
+  if (introComplete || introElapsedTime > animationOptionsDelay) {
+    introDrawAnimationOptions();
+  }
+}
+
+function introFadeOut() {
+  let fadeOutStartTime = millis();
+  let fadeOutDuration = 1000;
+  let fadeOutInterval = setInterval(() => {
+    let fadeOutElapsedTime = millis() - fadeOutStartTime;
+    let fadeOutRatio = map(fadeOutElapsedTime, 0, fadeOutDuration, 1, 0);
+    background(0, 255 * fadeOutRatio);
+    if (fadeOutElapsedTime >= fadeOutDuration) {
+      clearInterval(fadeOutInterval);
+    }
+  }, 16);
+}
+
+function showAnimationOptions() {
+  animations.forEach((animation, index) => {
+    let y = height / 2 + 100 + 30 * index;
+    fill(255);
+    text(animation.name, width / 2, y);
+  });
+}
+
+function introDrawAnimationOptions() {
+  textAlign(CENTER, CENTER);
+  textSize(24);
+  noStroke();
+
+  animations.forEach((animation, index) => {
+    let y = height / 2 + 100 + 30 * index;
+    fill(255);
+    text(animation.name, width / 2, y);
+
+    if (mouseX > width / 2 - 100 && mouseX < width / 2 + 100 && mouseY > y - 12 && mouseY < y + 12) {
+      fill(255, 50);
+      rect(width / 2, y, 200, 24);
+    }
+  });
+}
+
+function introMouseClicked() {
+  animations.forEach((animation, index) => {
+    let y = height / 2 + 100 + 30 * index;
+    if (mouseX > width / 2 - 100 && mouseX < width / 2 + 100 && mouseY > y - 12 && mouseY < y + 12) {
+      handleAnimationSelection(animation.name);
+    }
+  });
+}
+
+function handleAnimationSelection(name) {
+  if(animations.find(animation => animation.name === name) === undefined) {
+    console.error(`Animation "${name}" not found.`);
+    return;
+  }
+
+  let selection = animations.find(animation => animation.name === name);
+
+  animation = new selection.class();
+
+  introFadeOut();
+  appState = "main";
+}
